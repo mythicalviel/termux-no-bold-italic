77ce68d9774f995ca35208d9d47bc613469fb37e
mythicalviel <mystichasawoken@gmail.com>
2026-09-13 17:19:56 +0700

    Revert "Disable bold and italic text rendering"
    This reverts commit 9c3b14d59119df7398f507ebc1ef9d3667f31fec.

--- terminal-emulator/src/main/java/com/termux/terminal/TerminalEmulator.java
+++ terminal-emulator/src/main/java/com/termux/terminal/TerminalEmulator.java
@@ -2479,11 +2479,11 @@ public final class TerminalEmulator {
                 mBackColor = TextStyle.COLOR_INDEX_BACKGROUND;
                 mEffect = 0;
             } else if (code == 1) {
-                // Bold disabled.
+                mEffect |= TextStyle.CHARACTER_ATTRIBUTE_BOLD;
             } else if (code == 2) {
                 mEffect |= TextStyle.CHARACTER_ATTRIBUTE_DIM;
             } else if (code == 3) {
-                // Italic disabled.
+                mEffect |= TextStyle.CHARACTER_ATTRIBUTE_ITALIC;
             } else if (code == 4) {
                 if (i + 1 <= mArgIndex && ((mArgsSubParamsBitSet & (1 << (i + 1))) != 0)) {
                     // Sub parameter, see https://sw.kovidgoyal.net/kitty/underlines/
